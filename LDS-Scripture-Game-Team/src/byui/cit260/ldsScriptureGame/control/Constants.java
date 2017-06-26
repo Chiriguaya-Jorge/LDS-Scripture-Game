d3f9c95202df9784b9729d25b641d9b76b3a8686
hp <hp@JechirgHP.cigcpe>
2017-06-26 12:40:59 -0500

    TEAM WORK

--- LDS-Scripture-Game-Team/src/byui/cit260/ldsScriptureGame/control/Constants.java
+++ LDS-Scripture-Game-Team/src/byui/cit260/ldsScriptureGame/control/Constants.java
@@ -7,13 +7,13 @@ package byui.cit260.ldsScriptureGame.control;
 
 /**
  *
- * @author Jorge Chiriguaya
+ * @author Team Work
  */
 public class Constants {
-    public final static int NUMBER_OF_INVENTORY_ITEMS = 15;
+    public final static int NUMBER_OF_INVENTORY_ITEMS = 14;
     public final static int NUMBER_OF_ACTORS = 6;
     
-    public final static int REGULAR_SCENE = 0;
+    public final static int START_SCENE = 0;
     public final static int TRAVEL_SCENE = 1;
     public final static int RESOURCE_SCENE = 2;
     public final static int JERUSALEM_SCENE = 3;
@@ -24,50 +24,52 @@ public class Constants {
     public final static int ITEM_GRAIN = 2;
     public final static int ITEM_LEGUME = 3;
     public final static int ITEM_MEAT = 4;
-    public final static int ITEM_WATER = 5;
-    public final static int ITEM_BOW = 6;
-    public final static int ITEM_ARROW = 7;
-    public final static int ITEM_SLING = 8;
-    public final static int ITEM_GOLD = 9;
-    public final static int ITEM_SILVER = 10;
-    public final static int ITEM_SWORD = 11;
+    public final static int ITEM_OIL = 5;
+    public final static int ITEM_WATER = 6;
+    public final static int ITEM_BOW = 7;
+    public final static int ITEM_ARROW = 8;
+    public final static int ITEM_SLING = 9;
+    public final static int ITEM_GOLD = 10;
+    public final static int ITEM_SILVER = 11;
+    public final static int ITEM_SWORD = 12;
 
     
     
     // SCENES TYPES
     public final static int START = 0;
-    public final static int LEHISTENT = 1;
-    public final static int LEATHER = 2;
-    public final static int BAG  = 3;
-    public final static int CAMEL = 4;
-    public final static int DESERT = 5;
-    public final static int HUNT = 6;
-    public final static int MEAT = 7;
-    public final static int WATER = 8;
-    public final static int JERUSALEM = 9;
-    public final static int CASTLOTS = 10;
-    public final static int FIRSTTRY = 11;
-    public final static int FIRSTFLEE = 12;
-    public final static int PERSUADE = 13;
-    public final static int HOUSE = 14;
-    public final static int GOLD = 15;
-    public final static int SILVER = 16;
-    public final static int PRECIOUSTHINGS = 17;
-    public final static int SECONDTRY = 18;
-    public final static int SECONDFLEE = 19;
-    public final static int WILDERNESS = 20;
-    public final static int CAVITYOFAROCK = 21;
-    public final static int SMITE = 22;
-    public final static int ANGEL = 23;
-    public final static int THIRDTRY = 24;
-    public final static int LABAN = 25;
-    public final static int CONSTRAINEDTOKILL = 26;
-    public final static int DRESSING = 27;
-    public final static int TREASURY = 28;
-    public final static int ZORAMKEYS = 29;
-    public final static int OUTTHEWALLS = 30;
-    public final static int OATH = 31;
-    public final static int FINISH = 32;
+    public final static int REDSEA = 1;
+    public final static int LEHISTENT = 2;
+    public final static int LEATHER = 3;
+    public final static int BAG  = 4;
+    public final static int CAMEL = 5;
+    public final static int DESERT = 6;
+    public final static int HUNT = 7;
+    public final static int MEAT = 8;
+    public final static int WATER = 9;
+    public final static int JERUSALEM = 10;
+    public final static int CASTLOTS = 11;
+    public final static int FIRSTTRY = 12;
+    public final static int FIRSTFLEE = 13;
+    public final static int PERSUADE = 14;
+    public final static int HOUSE = 15;
+    public final static int GOLD = 16;
+    public final static int SILVER = 17;
+    public final static int PRECIOUSTHINGS = 18;
+    public final static int SECONDTRY = 19;
+    public final static int SECONDFLEE = 20;
+    public final static int WILDERNESS = 21;
+    public final static int ROCKCAVITY = 22;
+    public final static int SMITE = 23;
+    public final static int ANGEL = 24;
+    public final static int THIRDTRY = 25;
+    public final static int LABAN = 26;
+    public final static int CONSTRAINEDTOKILL = 27;
+    public final static int DRESSING = 28;
+    public final static int TREASURY = 29;
+    public final static int ZORAMKEYS = 30;
+    public final static int OUTTHEWALLS = 31;
+    public final static int OATH = 32;
+    public final static int FINISH = 33;
 
     
     public final static double GRAIN_POUNDS_PER_CUBIC_FOOT = 48.18;
@@ -104,5 +106,5 @@ public class Constants {
     public final static int MAP_ROW_COUNT = 10;
     public final static int MAP_COLUMN_COUNT = 10;
     public final static int CHARACTER_COUNT = 6;
-    public final static int NUMBER_OF_SCENES = 33;
+    public final static int NUMBER_OF_SCENES = 34;
 }
---
+++ LDS-Scripture-Game-Team/src/byui/cit260/ldsScriptureGame/model/SceneType.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.ldsScriptureGame.model;
+
+/**
+ *
+ * @author Team Work
+ */
+public enum SceneType {
+    start,
+    redSea,
+    lehisTent,
+    preparation,
+    leather,
+    bag,
+    camel,
+    grains,
+    desert,
+    river,
+    salt,
+    water,
+    hunt,
+    meat,
+    riverCrossing,
+    jerusalem,
+    castLosts,
+    firstTry,
+    firstFlee,
+    persuade,
+    house,
+    gold,
+    silver,
+    preciousThings,
+    secondTry,
+    secondFlee,
+    wilderness,
+    rockcCavity,
+    smite,
+    angel,
+    thirdTry,
+    laban,
+    constrainedToKill,
+    dressing,
+    treasury,
+    zoramKeys,
+    outTheWalls,
+    oath,
+    finish;
+}
